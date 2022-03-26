203d5ed6b11e97103152995187ed2439973d7bf2
Dai Kawahara <skbsporta7@gmail.com>
2022-03-26 09:36:38 +0900

    init app project

---
+++ Swift-Concurrency-Training/Swift-Concurrency-Training/Swift_Concurrency_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Concurrency_TrainingApp.swift
+//  Swift-Concurrency-Training
+//
+//  Created by kawaharadai on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Concurrency_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
