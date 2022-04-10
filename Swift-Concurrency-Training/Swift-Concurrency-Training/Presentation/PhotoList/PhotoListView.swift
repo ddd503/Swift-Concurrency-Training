9c3bef2200e97e14166530af4875ee8d0fbab43f
Dai Kawahara <skbsporta7@gmail.com>
2022-04-11 08:58:07 +0900

    add photolist datasource

---
+++ Swift-Concurrency-Training/Swift-Concurrency-Training/Presentation/PhotoList/PhotoListView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoListView.swift
+//  Swift-Concurrency-Training
+//
+//  Created by kawaharadai on 2022/04/11.
+//
+
+import SwiftUI
+
+struct PhotoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhotoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoListView()
+    }
+}
---
+++ Swift-Concurrency-Training/Swift-Concurrency-Training/Presentation/PhotoList/PhotoListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoListViewModel.swift
+//  Swift-Concurrency-Training
+//
+//  Created by kawaharadai on 2022/04/11.
+//
+
+import Foundation
