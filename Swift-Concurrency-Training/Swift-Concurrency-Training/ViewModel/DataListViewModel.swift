9b50b1da78e30f718e1399a3d54f32fa83fd9cc4
Dai Kawahara <skbsporta7@gmail.com>
2022-10-08 11:49:43 +0900

    update DataListViewModel

--- Swift-Concurrency-Training/Swift-Concurrency-Training/ViewModel/DataListViewModel.swift
+++ Swift-Concurrency-Training/Swift-Concurrency-Training/ViewModel/DataListViewModel.swift
@@ -9,6 +9,7 @@ import Combine
 
 final class DataListViewModel: ObservableObject {
 
+    // 使い方決める
     enum State: String {
         case initialized
         case failed
@@ -16,4 +17,17 @@ final class DataListViewModel: ObservableObject {
     }
 
     @Published var dataList = ["テスト1", "テスト2", "テスト3"]
+
+    func onAppear() {
+        fetchTestData()
+    }
+
+    func pullToRefresh() {
+        fetchTestData()
+    }
+
+    private func fetchTestData() {
+        // repository経由でAPI叩いて適当なデータを受け取る
+        // dataListの値を更新する
+    }
 }
