6a6643efc63ee16aa1e8d0f1daf6731b60afc914
Dai Kawahara <skbsporta7@gmail.com>
2022-12-03 21:18:48 +0900

    add test code

--- Swift-Concurrency-Training/Swift-Concurrency-TrainingTests/Model/UserAPIClientImplTest.swift
+++ Swift-Concurrency-Training/Swift-Concurrency-TrainingTests/Model/UserAPIClientImplTest.swift
@@ -7,9 +7,21 @@
 
 import XCTest
 import Mockingjay
+@testable import Swift_Concurrency_Training
 
 class UserAPIClientImplTest: XCTestCase {
 
-    func test_perform_response_normal() {}
+    // errorをスローするテストの場合はasync throwsをつければcatchしてくれる
+    func test_perform_response_normal() async throws {
+        let apiClient = UserAPIClientImpl()
+        let dummyReqest = URLRequest(url: URL(string: "http://localhost:3000/v1/users")!)
+        let reqestUrlString = "http://localhost:3000/v1/users"
+        let jsonFilePath = Bundle(for: type(of: self)).path(forResource: "dummy_users", ofType: "json")!
+        let responseData = try! Data(contentsOf: URL(fileURLWithPath: jsonFilePath), options: .mappedIfSafe)
+        stub(http(.get, uri: reqestUrlString), jsonData(responseData))
+        // エラースローした場合にどうなる？
+        let response = try await apiClient.perform(request: dummyReqest)
+        XCTAssertEqual(response.users.count, 2)
+    }
 
 }
