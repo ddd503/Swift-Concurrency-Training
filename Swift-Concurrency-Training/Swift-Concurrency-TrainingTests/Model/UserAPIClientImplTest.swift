fc9a7fb3efe4a64eb9cac5c35a4255b701c1a1e2
Dai Kawahara <skbsporta7@gmail.com>
2022-12-17 09:52:17 +0900

    add user api client test

--- Swift-Concurrency-Training/Swift-Concurrency-TrainingTests/Model/UserAPIClientImplTest.swift
+++ Swift-Concurrency-Training/Swift-Concurrency-TrainingTests/Model/UserAPIClientImplTest.swift
@@ -24,4 +24,27 @@ class UserAPIClientImplTest: XCTestCase {
         XCTAssertEqual(response.count, 3)
     }
 
+    func test_perform_response_error() {
+        let expectation = self.expectation(description: "任意のerrorを取得できること")
+        let apiClient = UserAPIClientImpl()
+        let testRequest = URLRequest(url: URL(string: "http://localhost:3000/v1/users")!)
+        let jsonFilePath = Bundle(for: type(of: self)).path(forResource: "dummy_users", ofType: "json")!
+        stub(condition: isPath("/v1/users")) { request in
+            HTTPStubsResponse(fileAtPath: jsonFilePath, statusCode: 400, headers: nil)
+        }
+
+        // errorをハンドリングするときはcatchで拾う
+        Task {
+            do {
+                _ = try await apiClient.perform(request: testRequest)
+            } catch let error as NSError {
+                XCTAssertEqual(error.code, 400)
+                XCTAssertEqual(error.domain, "out of statusCode range")
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.1)
+    }
+
 }
