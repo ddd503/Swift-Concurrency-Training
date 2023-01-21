e7ec9c215bd2df5b40567c479027f2ccdef98543
Dai Kawahara <skbsporta7@gmail.com>
2023-01-21 21:13:43 +0900

    add test file

---
+++ Swift-Concurrency-Training/Swift-Concurrency-TrainingTests/ViewModel/UserListViewModelTest.swift
@@ -0,0 +1,20 @@
+//
+//  UserListViewModelTest.swift
+//  Swift-Concurrency-TrainingTests
+//
+//  Created by kawaharadai on 2023/01/21.
+//
+
+import XCTest
+
+class UserListViewModelTest: XCTestCase {
+
+    func test_onAppear() {}
+
+    func test_pullToRefresh() {}
+
+    func test_onTapReloadButton() {}
+
+    func test_cancel() {}
+
+}
